f3c8fa97604fa258257cdf44dd447715325e826f
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-18 19:42:38 -0300

    finished project

---
+++ 90sites-challenge/03-carroseldeimagens/script.js
@@ -0,0 +1,7 @@
+document.querySelector("#items").addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    event.target.scrollBy(300, 0);
+  } else {
+    event.target.scrollBy(-300, 0);
+  }
+});
