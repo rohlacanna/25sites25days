c1014c0100da504a836f025bb231b1c89a323acb
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-20 23:27:15 -0300

    creation of the red, yellow and green buttons

--- 90sites-challenge/05-semaforo/script.js
+++ 90sites-challenge/05-semaforo/script.js
@@ -2,7 +2,13 @@ const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 
 const trafficLight = (event) => {
-  console.log(event);
+  turnOn[event.target.id]();
+};
+
+const turnOn = {
+  red: () => (img.src = "https://i.ibb.co/h9x75bD/vermelho.png"),
+  yellow: () => (img.src = "https://i.ibb.co/H7qp1MJ/amarelo.png"),
+  green: () => (img.src = "https://i.ibb.co/hYr39z9/verde.png"),
 };
 
 buttons.addEventListener("click", trafficLight);
