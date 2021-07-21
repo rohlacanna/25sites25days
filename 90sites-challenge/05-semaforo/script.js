d659f0cf80ebfbc2d0379d24d446c6b9e2046ffe
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-20 23:47:00 -0300

    creation of the automatic button

--- 90sites-challenge/05-semaforo/script.js
+++ 90sites-challenge/05-semaforo/script.js
@@ -1,14 +1,35 @@
 const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 
+let colorIndex = 0;
+
 const trafficLight = (event) => {
   turnOn[event.target.id]();
+};
+
+const nextIndex = () => (colorIndex = colorIndex < 2 ? ++colorIndex : 0);
+
+/* const nextIndex = () => {
+  if (colorIndex < 2) {
+    colorIndex++;
+  } else {
+    colorIndex = 0;
+  }
+}; */
+
+const changeColor = () => {
+  const colors = ["red", "yellow", "green"];
+  const color = colors[colorIndex];
+  turnOn[color]();
+
+  nextIndex();
 };
 
 const turnOn = {
   red: () => (img.src = "https://i.ibb.co/h9x75bD/vermelho.png"),
   yellow: () => (img.src = "https://i.ibb.co/H7qp1MJ/amarelo.png"),
   green: () => (img.src = "https://i.ibb.co/hYr39z9/verde.png"),
+  automatic: () => setInterval(changeColor, 1000),
 };
 
 buttons.addEventListener("click", trafficLight);
