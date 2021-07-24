338f82c0d885196302ac05d3a077b0ee11a01f1d
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-24 16:50:38 -0300

    feat: input focus function created in script

---
+++ 90sites-challenge/09-teladelogin/script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+});
