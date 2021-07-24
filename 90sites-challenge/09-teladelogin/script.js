c00d553f98187202b0818273f83bfbdee66ba9c5
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-24 16:54:22 -0300

    feat: created blur function

--- 90sites-challenge/09-teladelogin/script.js
+++ 90sites-challenge/09-teladelogin/script.js
@@ -5,6 +5,15 @@ function focusFunc() {
   parent.classList.add("focus");
 }
 
+function blurFunc() {
+  let parent = this.parentNode.parentNode;
+
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
