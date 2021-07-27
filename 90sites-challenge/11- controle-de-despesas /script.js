f7ec96347a325933a9e48b45286755567d230cd0
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-26 21:47:25 -0300

    feat: adds updateBalanceValues function functionality

--- 90sites-challenge/11- controle-de-despesas /script.js
+++ 90sites-challenge/11- controle-de-despesas /script.js
@@ -1,1 +1,43 @@
-// Script
+const transactionsUl = document.querySelector('#transactions');
+
+const dummyTransactions = [
+  { id: 1, name: 'Bolo de pote da Eleni', amount: -12 },
+  { id: 2, name: 'Salário', amount: 300 },
+  { id: 3, name: 'Torta de frango', amount: -10 },
+  { id: 4, name: 'Piano', amount: 150 },
+];
+
+const addTransactionIntoDOM = (transaction) => {
+  const operator = transaction.amount < 0 ? '-' : '+';
+  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+  const amountWidthoutOperator = Math.abs(transaction.amount);
+  const li = document.createElement('li');
+
+  li.classList.add(CSSClass);
+  li.innerHTML = `
+  ${transaction.name} <span>${operator} R$ ${amountWidthoutOperator}</span>
+    <button class="delete-btn">x</button>
+  `;
+  transactionsUl.append(li);
+};
+
+const updateBalanceValues = () => {
+  const transactionsAmounts = dummyTransactions.map(
+    (transaction) => transaction.amount
+  );
+  const total = transactionsAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+    .toFixed(2);
+  const income = transactionsAmounts
+    .filter((value) => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2);
+  console.log(income);
+};
+
+const init = () => {
+  dummyTransactions.forEach(addTransactionIntoDOM);
+  updateBalanceValues();
+};
+
+init();
