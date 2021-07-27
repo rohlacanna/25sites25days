8dd3da2c0208dac78df308535f45423a3a0107e4
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-26 21:56:33 -0300

    add: entering information in the DOM

--- 90sites-challenge/11- controle-de-despesas /script.js
+++ 90sites-challenge/11- controle-de-despesas /script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions');
+const incomeDisplay = document.querySelector('#money-plus');
+const expenseDisplay = document.querySelector('#money-minus');
+const balanceDisplay = document.querySelector('#balance');
 
 const dummyTransactions = [
   { id: 1, name: 'Bolo de pote da Eleni', amount: -12 },
@@ -36,7 +39,10 @@ const updateBalanceValues = () => {
     .filter((value) => value < 0)
     .reduce((accumulator, value) => accumulator + value, 0)
     .toFixed(2);
-  console.log(expense);
+
+  balanceDisplay.textContent = `R$ ${total}`;
+  incomeDisplay.textContent = `R$ ${income}`;
+  expenseDisplay.textContent = `R$ ${expense}`;
 };
 
 const init = () => {
