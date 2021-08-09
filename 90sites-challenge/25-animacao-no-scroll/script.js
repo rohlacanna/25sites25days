c9b34666cc451f19f3aee99d4db154fd625b243b
Rômulo Silva <romulo_lss@hotmail.com>
2021-08-09 13:29:18 -0300

    add: script for scroll effect

--- 90sites-challenge/25-animacao-no-scroll/script.js
+++ 90sites-challenge/25-animacao-no-scroll/script.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + 'px';
+  moon.style.top = value * 1.25 + 'px';
+  mountains_behind.style.top = value * 0.5 + 'px';
+  mountains_front.style.top = value * 0 + 'px';
+  text.style.marginRight = value * 4 + 'px';
+  text.style.marginTop = value * 1.5 + 'px';
+  btn.style.marginTop = value * 1.5 + 'px';
+  header.style.top = value * 0.5 + 'px';
+});
